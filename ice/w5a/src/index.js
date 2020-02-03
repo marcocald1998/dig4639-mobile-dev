3b112ce4c82fecbcfb2963cc810dac4427893a2a
Marco Calderon <marcocald1998@gmail.com>
2020-02-03 10:18:01 -0500

    Co-authored-by: mkbarron <mkbarron@knights.ucf.edu>

--- ice/w5a/src/index.js
+++ ice/w5a/src/index.js
@@ -1,16 +1,33 @@
+let element = undefined;
+let taskInput
 function runOnLoad()
 {
     // Create a container for us
-    let element = document.createElement("div");
+    element = document.createElement("div");
     element.id = "container";
     document.body.appendChild(element);
 
     // Handle adding a new task
     var addTaskButton = document.getElementById("addTask");
     addTaskButton.addEventListener("click", onClick)
+    var taskInput = document.getElementById("taskText");
 }
 function onClick() {
     console.log("clicked!");
+    let newTask = document.createElement("div");
+    let input = document.createElement("input");
+    input.type = "checkbox";
+    let span = document.createElement("span");
+    span.innerHTML = taskInput.value;
+    newTask.appendChild(input)
+    newTask.appendChild(span);
+    function onChange() {
+        console.log(span.innerHTML);
+    }
+    inpput.addEventListener("change", onChange);
+   // newTask.innerHTML = `<input type='checkbox'></input><span> ${taskInput.value} </span>`;
+    element.appendChild(newTask);
+    
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
