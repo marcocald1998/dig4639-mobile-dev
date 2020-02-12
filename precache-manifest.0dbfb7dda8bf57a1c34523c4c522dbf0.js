a5dbb62f6bdc6b9e39fba00147ee7b3f0ee3ded7
Marco Calderon <marcocald1998@gmail.com>
2020-02-12 10:05:50 -0500

    Updates

---
+++ precache-manifest.0dbfb7dda8bf57a1c34523c4c522dbf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a260359f5cea6295e73c8bb5d3293fc0",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "d756001e8d53a028c89d",
+    "url": "/dig4639-mobile-dev/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d756001e8d53a028c89d",
+    "url": "/dig4639-mobile-dev/static/js/main.928116ce.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
