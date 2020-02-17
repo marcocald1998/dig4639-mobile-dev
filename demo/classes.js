c1b59634d2be16044b382444917aa7759e3b7a7c
Marco Calderon <marcocald1998@gmail.com>
2020-02-16 20:31:57 -0500

    tic tac toe

--- demo/classes.js
+++ demo/classes.js
@@ -2,38 +2,38 @@ class Pet {
     petSound;
     petFeeding;
 
-    constructor(species) {
-        this.species = species;
+    constructor (species) {
+      this.species = species
     }
 
-    speak()  {
-        return "No words";
+    speak () {
+      return 'No words'
     }
 }
 
 class Cat extends Pet {
-    constructor(props){
-        super("cat");
-        this.props = props;
-    }
-    clickHandler = parameter =>  parameter+this.props.name;
+  constructor (props) {
+    super('cat')
+    this.props = props
+  }
 
-    speak(){
-        this.clickHandler();
-        return `${super.speak()} Meow, I am a ${this.species}`;
+    clickHandler = parameter => parameter + this.props.name;
+
+    speak () {
+      this.clickHandler()
+      return `${super.speak()} Meow, I am a ${this.species}`
     }
 }
 
-function makePetSpeak(pet){
-    console.log(pet.speak());
-
+function makePetSpeak (pet) {
+  console.log(pet.speak())
 }
 
-var myPet = new Pet();
-makePetSpeak(myPet);
+var myPet = new Pet()
+makePetSpeak(myPet)
 
-var myCat = new Cat({name: "Mr. Whiskers"});
-makePetSpeak(myCat);
+var myCat = new Cat({ name: 'Mr. Whiskers' })
+makePetSpeak(myCat)
 
-var newFunc = myCat.clickHandler;
-console.log(newFunc("My name is "));
+var newFunc = myCat.clickHandler
+console.log(newFunc('My name is '))
