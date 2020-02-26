d5a909225d17d782d9a443eb37bd8a0bacd3c758
Marco Calderon <marcocald1998@gmail.com>
2020-02-26 10:22:35 -0500

    #98

--- demo/w8a/todo-react/src/App.js
+++ demo/w8a/todo-react/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import todoList from './todoList.json'
 
-function TodoItem(props) {
-  return <p className='card'>{props.content}</p>
+class TodoItem extends React.Component {
+  render() {
+    return <p className='card'>{this.props.content}<input type="button" value="x"
+      onClick={() => this.props.removeTask(this.props.id)}/></p>
+  }
 }
 
 class App extends React.Component {
@@ -13,11 +16,17 @@ class App extends React.Component {
       todoList: todoList,
       showOnlyUncompletedTasks: false
   }
+  this.currentID = 4
+}
+
+removeTask(taskID) {
+  let newList = this.state.todoList.filter((task) => (task.id !== taskID))
+  this.setState({ todoList:newList })
 }
 
   addTask() {
     let newtask =
-    { "content": this.refs.taskContent.value, "priority": 3, "completed": false}
+    { "content": this.refs.taskContent.value, "priority": this.refs.priority.value, "completed": false}
     let currentList = this.state.todoList
     currentList.push(newtask)
     this.setState({ todoList: currentList })
@@ -28,6 +37,12 @@ class App extends React.Component {
     <>
     <div className="inputTask">
     <input type="text" ref="taskContent"/>
+    <label for="numbers">Choose priority:</label>
+    <select ref="priority" name="priority">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+    </select>
     <input type="button" value="Add Task" onClick={() => this.addTask()}/>
     <br />
     <input type="checkbox" ref="completedFilter"
@@ -39,7 +54,8 @@ class App extends React.Component {
       this.state.todoList.filter((v) => v.completed)
        :
       this.state.todoList)
-      .map((v, i) => <TodoItem key={i} priority={v.priority}
+      .map((v, i) => <TodoItem id={v.id} key={i} priority={v.priority} 
+      removeTask={(id) => this.removeTask(id)}
       content={v.content} completed={v.completed}/>)}
       </>
       );
